ac7f7cb025e3086f0c5237fef7af7cb00687d23e
Dan Manning <dmanning23@hotmail.com>
2015-09-21 22:22:46 -0500

    got ios working

--- AdsPCL/AdsPCL.Android/CustomBannerRenderer.cs
+++ AdsPCL/AdsPCL.Android/CustomBannerRenderer.cs
@@ -3,6 +3,7 @@ using Android.CustomRenders;
 using Android.Gms.Ads;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+using System;
 
 [assembly: ExportRenderer(typeof(MyBanner), typeof(CustomBannerRenderer))]
 namespace Android.CustomRenders
@@ -13,15 +14,24 @@ namespace Android.CustomRenders
         {
             base.OnElementChanged(e);
 
-            if (e.OldElement == null)
-            {
-                AdView ad = new AdView(this.Context);
-                ad.AdSize = AdSize.Banner;
-				ad.AdUnitId = "ca-app-pub-8228485892439970/3183360843";
-                var requestbuilder = new AdRequest.Builder();
-                ad.LoadAd(requestbuilder.Build());
-                this.SetNativeControl(ad);
-            }
+			try
+			{
+				if (e.OldElement == null)
+				{
+					AdView ad = new AdView(this.Context);
+					ad.AdSize = AdSize.Banner;
+					ad.AdUnitId = "ca-app-pub-8228485892439970/3183360843";
+					var requestbuilder = new AdRequest.Builder();
+
+					//This line throws Java.Lang.NoClassDefFoundError ???
+					ad.LoadAd(requestbuilder.Build());
+
+					this.SetNativeControl(ad);
+				}
+			}
+			catch (Exception ex)
+			{
+			}
         }
     }
 }
